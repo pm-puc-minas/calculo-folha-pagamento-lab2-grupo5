fb240af22386f56219d9fd582542e597d9f37623
HelenoidJ <joaoheleno971@gmail.com>
2025-09-23 21:59:27 -0300

    feat: Criação da logica dos vales e adição da logica do metodo de Gestor

--- folha_pagamento_spring/src/main/java/folha_de_pagamento/model/Alimentacao.java
+++ folha_pagamento_spring/src/main/java/folha_de_pagamento/model/Alimentacao.java
@@ -3,6 +3,21 @@ package folha_de_pagamento.model;
 import java.math.BigDecimal;
 
 public class Alimentacao extends Vale {
+
+    private double valorAlimentacao;
+
+    public Alimentacao() {}
+
+    public Alimentacao(double valorAlimentacao) {
+        this.valorAlimentacao = valorAlimentacao;
+    }
     
-    public BigDecimal valorAlimentacao(Funcionario funcionario) { return null; }
+    @Override
+    public BigDecimal calcularVale(Funcionario funcionario) { 
+        
+        int diasTrabalhadosNoMes = funcionario.getDiasTrabalhadosNoMes();
+        double valorVale = valorAlimentacao * diasTrabalhadosNoMes;
+        
+        return new BigDecimal(valorVale);
+     }
 }
