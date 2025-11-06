389627586d36ebf85ceca3d6e5dc17ac78e5bc9b
Bzinnnn <bernardo.prado88@gmail.com>
2025-11-06 15:48:12 -0300

    commit front

---
+++ doc/frontend/Front/js/main.js
@@ -0,0 +1,43 @@
+
+function navegarSuave(url) {
+  const content = document.querySelector(".content");
+  if (content) {
+    content.classList.add("fade-out");
+  }
+
+  setTimeout(() => {
+    window.location.href = url;
+  }, 250);
+}
+
+function logout() {
+  localStorage.removeItem("auth");
+  const content = document.querySelector(".content");
+  if (content) {
+    content.classList.add("fade-out");
+  }
+  setTimeout(() => {
+    window.location.href = "login.html";
+  }, 250);
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const content = document.querySelector(".content");
+  if (content) {
+    content.classList.remove("fade-out");
+  }
+
+  
+  document.querySelectorAll(".sidebar a").forEach((link) => {
+    const href = link.getAttribute("href");
+
+    
+    if (href && !link.classList.contains("logout")) {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        navegarSuave(href);
+      });
+    }
+  });
+});
